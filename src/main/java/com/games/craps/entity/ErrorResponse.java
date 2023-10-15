deb2b74f1effa5609fa7a487037ee7916a1829c8
INestorov <ivan.nestorov1401@gmail.com>
2023-10-15 23:19:10 +0300

    Forgot default constructor.

--- src/main/java/com/games/craps/entity/ErrorResponse.java
+++ src/main/java/com/games/craps/entity/ErrorResponse.java
@@ -5,6 +5,8 @@ import java.util.List;
 public class ErrorResponse {
     private List<String> message;
 
+    public ErrorResponse(){}
+
     public ErrorResponse(List<String> message) {
         this.message = message;
     }
