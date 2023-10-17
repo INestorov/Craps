8670a681e77c2ca311900a35c03da216c3b501b8
INestorov <ivan.nestorov1401@gmail.com>
2023-10-17 13:07:48 +0300

    Added tests for CrapsGameLogic.

--- src/test/java/com/games/craps/gamelogic/CarpsGameLogicTest.java
+++ src/test/java/com/games/craps/gamelogic/CarpsGameLogicTest.java
@@ -3,15 +3,36 @@ package com.games.craps.gamelogic;
 import com.games.craps.entity.GameRequest;
 import com.games.craps.entity.GameResponse;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
-public class CarpsGameLogicTest {
+class CarpsGameLogicTest {
+    @Test
+    void testPlayOneGame() {
+        GameRequest gameRequest = new GameRequest(new BigDecimal("2.00"), "craps");
+
+        ResponseEntity responseEntity = CarpsGameLogic.playOneGame(gameRequest);
+
+        assertThat(responseEntity.getStatusCode(), is(HttpStatus.OK));
+    }
 
     @Test
-    public void playOneRoundTest() {
+    void testPlayMultipleGames() {
+        GameRequest gameRequest = new GameRequest(new BigDecimal("2.00"), "craps");
+
+        ResponseEntity responseEntity = CarpsGameLogic.playMultipleGames(5, gameRequest);
+
+        assertThat(responseEntity.getStatusCode(), is(HttpStatus.OK));
+    }
+
+    @Test
+    void playOneRoundTest() {
         GameRequest gameRequest = new GameRequest(new BigDecimal("2.00"), "craps");
 
         GameResponse gameResponse = CarpsGameLogic.playOneRound(gameRequest);
